533ca788cb255d7616f564bc26c0610cc5956944
goohst8 <94585651+goohst8@users.noreply.github.com>
2022-10-04 12:27:42 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "732285322662133760"; // your league ID
-export const leagueName = "Freakless 21"; // your league name
+export const leagueID = "850796037718261760"; // your league ID
+export const leagueName = "Freakless"; // your league name
 export const dues = 200; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
